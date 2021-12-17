8ab1d67af49e7d07550bf2b0f140d02a325144ef
Barry Liu <barry.y.liu@hotmail.com>
2021-12-17 23:56:33 +0800

    [U] Update

--- silvaengine_auth/main.py
+++ silvaengine_auth/main.py
@@ -310,9 +310,10 @@ class Auth(object):
     ):
         try:
             if type(role_users_map) is dict and len(role_users_map):
-                group_ids = None
+                # group_ids = None
 
-                if relationship_type != RoleRelationshipType.ADMINISTRATOR.value:
+                # if relationship_type != RoleRelationshipType.ADMINISTRATOR.value:
+                if group_id:
                     group_ids = group_id if type(group_id) is list else [group_id]
 
                 for role_id, user_ids in role_users_map.items():
